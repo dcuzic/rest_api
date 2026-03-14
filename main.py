cf12bbfefd6befe1508c08f8620392d00bf74c59
dcuzic <dcuzic1@gmail.com>
2026-03-14 20:38:37 +0000

    deleted not needed old code, moved sql connection to every function, fixed create_booking, added delete_booking function, added all_bookings function, WIP: created search_booking function, working on returning info based on id, create_table

--- main.py
+++ main.py
@@ -1,86 +1,75 @@
 from fastapi import FastAPI, HTTPException
-from models import Booking
 import sqlite3
-
-count_file = "count.txt"
 
 app = FastAPI()
 
-bookings = []
-id_count = 1
-
-# DATABASE
-conn = sqlite3.connect("database.db")
-cursor = conn.cursor()
-
+# maybe doesnt work
 def create_table():
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
     cursor.execute("""
                    CREATE TABLE IF NOT EXISTS bookings (
-                   id INTEGER PRIMARY KEY AUTOINCREMENT
-                   name TEXT NOT NULL
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   name TEXT NOT NULL,
                    date TEXT NOT NULL
                    )              
                    """)
     conn.commit()
+    conn.close()
 
-# new booking - line 29, in create_booking
-#   cursor.execute("""
-#                  ^^^
-# TypeError: 'str' object is not callable
+create_table()
+
+# adds booking, works
 @app.post("/bookings/")
 def create_booking(booking_name, booking_date):
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
     cursor.execute("""
                    INSERT INTO bookings (name, date)
-                   VALUES (?, ?)"""
+                   VALUES (?, ?)""",
                    (booking_name, booking_date)
                    )
+    conn.commit()
+    conn.close()
+    return {"booking successfully created"}
 
-# posts new booking - works
-@app.post("/booking/")
-def create_booking(booking: Booking):
-    booking_dict = booking.dict()
-    global id_count
-    booking_id = id_count
-    ids = id_count =+ 1
-    def increment_counter():
-        try:
-            with open(count_file, "r") as s:
-                count = int(s.read())
-        except FileNotFoundError:
-            count = 0
-        count += 1
-        with open(count_file, "w") as s:
-            s.write(str(count))
-        return count
-    booking_dict["id"] = increment_counter()
-    bookings.append(booking_dict)
-    print(booking_dict)
-    return booking_dict
+# deletes booking from database, works
+@app.delete("/bookings")
+def delete_booking(booking_id):
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+    cursor.execute("""DELETE FROM bookings WHERE id = ?""", (booking_id,))
+    conn.commit()
+    conn.close()
+    return {f"booking no {booking_id} successfully deleted"}
 
-# gets a list of all the bookings - works
-@app.get("/booking/")
+# returns all bookings in database, works
+@app.get("/bookings/")
 def all_bookings():
-    cursor.execute("""FROM """)
+    conn = sqlite3.connect("database.db")
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM bookings")
+    rows = cursor.fetchall()
+    data = [dict(row) for row in rows]
+    conn.close()
+    return {
+        "message": "here are all the bookings in the system right now",
+        "data": data
+    }
 
-# returns booking details by booking ID - works
-@app.get("/booking/{booking_id}")
-def id(booking_id: int):
+# needs fixing, working on how to make sqlite check if input id is in database,
+# if yes return id details 
+@app.get("/booking/{booking_id}/")
+def search_booking(booking_id):
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
     for booking in bookings:
-        if booking_id == booking["id"]:
-            print(type(booking_id))
-            return booking
-    else:
-        raise HTTPException(status_code=404, detail="Booking not found")
-        
-# deletes booking - works
-@app.delete("/bookings/{booking_id}")
-def delete_booking(booking_id: int):
-    for booking in bookings:
-        if booking["id"] == booking_id:
-            bookings.remove(booking)
-            return {f"Booking {booking_id} deleted successfully"}
-    
-    else:
-        return HTTPException(status_code=404, detail="Booking not found")
-
+        if booking["id"] in bookings:
+            cursor.execute("SELECT * FROM bookings WHERE id = ?", (booking_id,))
+            booking_info = cursor.fetchall()
+        else:
+            raise HTTPException(status_code=404, detail="booking not found")
+    conn.close()
+    return booking_info
 
