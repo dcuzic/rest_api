b3725f10c3a9fa1f390df30c7ff550b9892283f9
dcuzic <dcuzic1@gmail.com>
2026-04-05 21:10:35 +0100

    added conversion to bytes in hash_password, verify_password, WIP: register, login defs

--- auth.py
+++ auth.py
@@ -2,6 +2,7 @@ from jose import jwt
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
 from fastapi import FastAPI
+import bcrypt
 from database import db_conn
 
 
@@ -25,17 +26,20 @@ app = FastAPI()
 
 SECRET_KEY = "1e038aa16fdc2c17a4eab16b85085d58e4c52617973ca8573aa828a0070a8c7e"
 ALGORITHM = "HS256"
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated=["auto"])
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-
+# password
 def hash_password(password: str):
-    return pwd_context.hash(password)
+    password_bytes = password.encode("utf-8")[:72]
+    hashed_by = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
+    return hashed_by.decode("utf-8")
 
-def verify_password(plain, hashed):
-    return pwd_context.verify(plain, hashed)
+def verify_password(plain: str, hashed: str):
+    password_bytes = plain.encode("utf-8")[:72]
+    return bcrypt.checkpw(password_bytes, hashed.encode("utf-8"))
 
-
+# token
 def create_token(data: dict):
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(hours=1)
@@ -44,15 +48,29 @@ def create_token(data: dict):
 
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
-
-@app.post("/register")
-def user_register(username, password):
+# login
+def login(username, password):
     conn = db_conn()
     cursor = conn.cursor()
 
-    cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
+    cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
+    user = cursor.fetchone()
+    conn.close()
+
+    if user and verify_password(password, user[2]):
+        return create_token(user[0])
+    
+    return None
+
+# register - working
+def register(username: str, password: str):
+    conn = db_conn()
+    cursor = conn.cursor()
+
+    hashed = hash_password(password)
+    cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, hashed))
 
     conn.commit()
     conn.close()
-    
 
+    return f"User named {username} successfully registered"
